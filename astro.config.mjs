c783b6f1bd6661a69af3907d049bbc4d534162b5
Alejandra Codocedo <lettar.hyuga@gmail.com>
2026-05-01 19:27:29 -0400

    config cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare'
 
 import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {enabled: true}
+  }),
   integrations: [sanity({
       projectId: "o7ggtpu6",
       dataset: "production",
       useCdn: false, // for static builds
-    })]
+    })],
+  vite: {
+    ssr: {
+      external: ['node:buffer', 'node:path', 'node:stream', 'node:util']
+    }
+  }
 });
