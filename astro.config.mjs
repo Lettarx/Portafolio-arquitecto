571f547c63b8fe24863db747a8c220b547f35f76
Alejandra Codocedo <lettar.hyuga@gmail.com>
2026-05-01 23:47:45 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import sanity from '@sanity/astro';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    platformProxy: { enabled: true },
-  }),
-  integrations: [
-    sanity({
-      projectId: import.meta.env.PROJECTID_SANITY,
-      dataset: import.meta.env.DATASET_SANITY,
-      useCdn: true,
-    }),
-  ],
+  adapter: vercel(),
+  integrations: [sanity({
+    projectId: "o7ggtpu6",
+    dataset: "production",
+    useCdn: true,
+  })],
   vite: {
     ssr: {
-      external: ['node:buffer', 'node:path', 'node:stream', 'node:util'],
-    },
-  },
+      external: ['node:buffer', 'node:path', 'node:stream', 'node:util']
+    }
+  }
 });
