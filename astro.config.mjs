1d9f1db5f834c2612ff1880d1295896bfc097e29
Alejandra Codocedo <lettar.hyuga@gmail.com>
2026-05-01 21:19:17 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare'
-
+import cloudflare from '@astrojs/cloudflare';
 import sanity from '@sanity/astro';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    platformProxy: {enabled: true},
-    
+    platformProxy: { enabled: true },
   }),
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: {}
-    }
-  },
-  integrations: [sanity({
-      projectId: "o7ggtpu6",
-      dataset: "production",
-      useCdn: true, // for static builds
-    })],
+  integrations: [
+    sanity({
+      projectId: 'o7ggtpu6',
+      dataset: 'production',
+      useCdn: true,
+    }),
+  ],
   vite: {
     ssr: {
-      external: ['node:buffer', 'node:path', 'node:stream', 'node:util']
-    }
-  }
+      external: ['node:buffer', 'node:path', 'node:stream', 'node:util'],
+    },
+  },
 });
